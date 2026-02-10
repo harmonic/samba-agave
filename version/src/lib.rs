c68c160c9079f512b347ec5ccd3aaa21fc4e79b4
cavemanloverboy <caveycool@gmail.com>
2026-02-10 17:39:59 +0000

    harmonic fd client id

--- version/src/lib.rs
+++ version/src/lib.rs
@@ -28,6 +28,7 @@ pub enum ClientId {
     JitoLabs,
     Firedancer,
     Agave,
+    HarmonicFiredancer,
     // If new variants are added, update From<u16> and TryFrom<ClientId>.
     Unknown(u16),
 }
@@ -80,7 +81,7 @@ impl Default for Version {
             commit: unsafe { fdctl_commit_ref },
             feature_set,
             // Other client implementations need to modify this line.
-            client: u16::try_from(ClientId::Firedancer).unwrap(),
+            client: u16::try_from(ClientId::HarmonicFiredancer).unwrap(),
         }
     }
 }
@@ -115,6 +116,7 @@ impl From<u16> for ClientId {
             1u16 => Self::JitoLabs,
             2u16 => Self::Firedancer,
             3u16 => Self::Agave,
+            11u16 => Self::HarmonicFiredancer,
             _ => Self::Unknown(client),
         }
     }
@@ -129,7 +131,9 @@ impl TryFrom<ClientId> for u16 {
             ClientId::JitoLabs => Ok(1u16),
             ClientId::Firedancer => Ok(2u16),
             ClientId::Agave => Ok(3u16),
-            ClientId::Unknown(client @ 0u16..=3u16) => Err(format!("Invalid client: {client}")),
+            ClientId::HarmonicFiredancer => Ok(11u16),
+            ClientId::Unknown(client @ 0u16..=3u16)
+            | ClientId::Unknown(client @ 11u16) => Err(format!("Invalid client: {client}")),
             ClientId::Unknown(client) => Ok(client),
         }
     }
@@ -167,20 +171,22 @@ mod test {
         assert_eq!(ClientId::from(1u16), ClientId::JitoLabs);
         assert_eq!(ClientId::from(2u16), ClientId::Firedancer);
         assert_eq!(ClientId::from(3u16), ClientId::Agave);
-        for client in 4u16..=u16::MAX {
+        assert_eq!(ClientId::from(11u16), ClientId::HarmonicFiredancer);
+        for client in [4u16, 5u16, 10u16, 12u16] {
             assert_eq!(ClientId::from(client), ClientId::Unknown(client));
         }
         assert_eq!(u16::try_from(ClientId::SolanaLabs), Ok(0u16));
         assert_eq!(u16::try_from(ClientId::JitoLabs), Ok(1u16));
         assert_eq!(u16::try_from(ClientId::Firedancer), Ok(2u16));
         assert_eq!(u16::try_from(ClientId::Agave), Ok(3u16));
-        for client in 0..=3u16 {
+        assert_eq!(u16::try_from(ClientId::HarmonicFiredancer), Ok(11u16));
+        for client in [0u16, 1u16, 2u16, 3u16, 11u16] {
             assert_eq!(
                 u16::try_from(ClientId::Unknown(client)),
                 Err(format!("Invalid client: {client}"))
             );
         }
-        for client in 4u16..=u16::MAX {
+        for client in [4u16, 5u16, 10u16, 12u16] {
             assert_eq!(u16::try_from(ClientId::Unknown(client)), Ok(client));
         }
     }
